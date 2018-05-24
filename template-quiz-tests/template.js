8bcba1625c6996f990ddd4ee15d6f5bb9d98533f
xiaofan <1353815633@qq.com>
2018-05-25 00:27:18 +0800

    init template-quiz-tests

---
+++ template-quiz-tests/template.js
@@ -0,0 +1,11 @@
+function template(str, delims) {
+  // Fill this in
+  
+
+  return function (/* value...*//* repeat */) {
+
+    // remember console.log
+    return str; // template renderer usually returns a string
+  };
+
+}
